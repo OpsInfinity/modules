d70c8a164c3f471f49cec684d8de3a668d530f5a
Chowdary-Hari <chandrahari321@outlook.com>
2025-07-08 11:39:17 +0530

    Automated commit #1334

--- vpc/main.tf
+++ vpc/main.tf
@@ -15,17 +15,3 @@ module "dev-vpc" {
     default_route_table_id  = data.aws_route_table.main.id
     default_vpc_cidr        = data.aws_vpc.default.cidr_block
 }
-module "prod-vpc" {
-  source                 = "git::https://github.com/OpsInfinity/aws.git//vpc?ref=main"
-  env                    = "prod"
-  project-name           = "expense"
-  vpc_cidr               = "10.0.0.0/16"
-  azs                    = slice(data.aws_availability_zones.available.names, 0, 2)
-  public_subnet_cidrs    = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnet_cidrs   = ["10.0.11.0/24", "10.0.12.0/24"]
-  database_subnet_cidrs  = ["10.0.21.0/24", "10.0.22.0/24"]
-  account_no             = data.aws_caller_identity.current.account_id
-  default_vpc_id         = data.aws_vpc.default.id
-  default_route_table_id = data.aws_route_table.main.id
-  default_vpc_cidr       = data.aws_vpc.default.cidr_block
-}
